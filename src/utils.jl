fce9a39b626d6eb1bfaf281f3891bb2225c4fef8
Yueh-Hua Tu <a504082002@gmail.com>
2021-01-29 14:01:41 +0800

    add more helper functions

--- src/utils.jl
+++ src/utils.jl
@@ -5,6 +5,59 @@ export
 
 factorial_ij(i::Integer, j::Integer) = factorial(big(max(i,j))) / factorial(big(min(i,j)))
 
+z(x::Real, p::Real) = sqrt(2.)*(x + p*im)
+z(x::Vector{<:Real}, p::Vector{<:Real}) = z.(x, p')
+
+α(m::Integer, n::Integer) = n - m
+α(m::Vector{<:Integer}, n::Vector{<:Integer}) = α.(max.(m, n'), min.(m, n'))
+
+gaussian_function(x::Real, p::Real) = exp(-0.5 * abs2(z(x,p))) / π
+gaussian_function(x::Vector{<:Real}, p::Vector{<:Real}) = gaussian_function.(x,p')
+
+neg_one_to_power_of(i::Integer) = (i % 2 == 0) ? 1 : -1
+
+function combination_number(m::Integer, n::Integer)
+    if n ≥ m
+        return neg_one_to_power_of(m) / sqrt(factorial_ij(m, n))
+    else
+        return neg_one_to_power_of(n) / sqrt(factorial_ij(m, n))
+    end
+end
+
+combination_number(m::Vector{<:Integer}, n::Vector{<:Integer}) = combination_number.(m, n')
+
+function z_to_power(m::Integer, n::Integer, x::Real, p::Real)
+    if n ≥ m
+        return conj(z(x, p'))^(n - m)
+    else
+        return z(x, p')^(m - n)
+    end
+end
+
+function z_to_power(m::Vector{<:Integer}, n::Vector{<:Integer}, x::Vector{<:Real}, p::Vector{<:Real})
+    x = reshape(x, 1, 1, length(x))
+    p = reshape(p, 1, 1, 1, length(p))
+    z_to_power.(m, n', x, p)
+end
+
+function z_to_power(m::Vector{<:Integer}, n::Vector{<:Integer})
+    function z_to_power_xp(x::Vector{<:Real}, p::Vector{<:Real})
+        x = reshape(x, 1, 1, length(x))
+        p = reshape(p, 1, 1, 1, length(p))
+        z_to_power.(m, n', x, p)
+    end
+    return z_to_power_xp
+end
+
+function z_to_power(x::Vector{<:Real}, p::Vector{<:Real})
+    x = reshape(x, 1, 1, length(x))
+    p = reshape(p, 1, 1, 1, length(p))
+    function z_to_power_mn(m::Vector{<:Integer}, n::Vector{<:Integer})
+        z_to_power.(m, n', x, p)
+    end
+    return z_to_power_mn
+end
+
 function laguerre(n::Integer, α::Integer, x::Real)
     # by Horner's method
     laguerre_l = 1
@@ -37,13 +90,21 @@ end
 
 laguerre2(n::Integer, α::Integer) = x->laguerre2(n, α, x)
 
+function laguerre2(m::Integer, n::Integer, x::Real, p::Real)
+    if n ≥ m
+        return laguerre2(m, n - m, abs2([x, p]))
+    else
+        return laguerre2(n, m - n, abs2([x, p]))
+    end
+end
+
 function laguerre2(m::Vector{<:Integer}, n::Vector{<:Integer}, x::Vector{<:Real}, p::Vector{<:Real})
     x = reshape(x, 1, 1, length(x))
     p = reshape(p, 1, 1, 1, length(p))
     if n ≥ m
-        return laguerre2.(m, n' .- m, abs2.(x, p))
+        return laguerre2.(m, n' .- m, x, p)
     else
-        return laguerre2.(n', m .- n', abs2.(x, p))
+        return laguerre2.(n', m .- n', x, p)
     end
 end
 
@@ -57,9 +118,9 @@ function laguerre2(x::Vector{<:Real}, p::Vector{<:Real})
     p = reshape(p, 1, 1, 1, length(p))
     function laguerre_mn(m::Vector{<:Integer}, n::Vector{<:Integer})
         if n ≥ m
-            return laguerre2.(m, n' .- m, abs2.(x, p))
+            return laguerre2.(m, n' .- m, x, p)
         else
-            return laguerre2.(n', m .- n', abs2.(x, p))
+            return laguerre2.(n', m .- n', x, p)
         end
     end
     return laguerre_mn
