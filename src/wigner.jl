1801cbb9df9378dc282fc8cf1de4cdf755ce149c
Yueh-Hua Tu <a504082002@gmail.com>
2021-01-29 16:33:59 +0800

    refactor wigner

--- src/wigner.jl
+++ src/wigner.jl
@@ -2,24 +2,16 @@ export
     wigner,
     gen_wigner
 
-function wigner(m, n, x, p)
-    imag = 1im
-    if n < m
-        m, n = n, m
-        imag = -1im
-    end
-
-    w = 1 / pi
-    w *= exp(-(x^2 + p^2))
-    w *= (-1)^m
-    w *= sqrt(2^(n-m) / factorial_ij(m+1, n))
-    w *= (x - p*imag)^(n-m)
-    w *= laguerre(m, n-m)(2x^2 + 2p^2)
+function wigner(m::Integer, n::Integer, x::Real, p::Real)
+    w = gaussian_function(x, p)
+    w *= combination_number(m, n)
+    w *= z_to_power(m, n, x, p)
+    w *= laguerre(m, n, x, p)
 
     return w
 end
 
-wigner(m, n) = (x, p)->wigner(m, n, x, p)
+wigner(m::Integer, n::Integer) = (x, p)->wigner(m, n, x, p)
 
 mutable struct WignerFunction{M,T<:Integer}
     ρ::M
