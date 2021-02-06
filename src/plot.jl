47a83c24e6edf81b1d48afecc9ea23277fd86859
JingYu Ning <foldfelis@gmail.com>
2021-02-07 00:27:40 +0800

    fix deg

--- src/plot.jl
+++ src/plot.jl
@@ -82,7 +82,7 @@ function plot_wigner(
             ylabel=:P,
             cbrange=(-lim, lim),
             grid=:on,
-            view=(45, -45),
+            view=(45, 315),
             palette="defined (-$lim '#239DDB', 0 '#F0F0F0', $lim '#DB4044')"
         ),
     )
